7d0b8fe022ed5babd86d9fc5dc6542ffb91ae423
albello <kalbn@internet.ru>
2022-09-15 16:28:32 +0600

    test

--- main.py
+++ main.py
@@ -1,102 +1,13 @@
 from aiogram import types
 from aiogram.utils import executor
-from aiogram.types import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from config import bot, dp,Admin
-import logging
-import random
+from handlers import admin,callback,client,extra
 
 
-@dp. message_handler(commands=['start'])
-async def command_start(message: types. Message):
-    await message. answer(f'привет {message. from_user.id}')
-
-@dp. message_handler(commands=['quiz'])
-async def quiz1(message:types.Message):
-    markup = InlineKeyboardMarkup()
-    button_1 = InlineKeyboardButton('NEXT', callback_data='button_1')
-    markup. add(button_1)
-    question = "кто из них лучший?"
-    answer = [
-         'нурдин',
-         'али',
-         'все',
-         'вадим',
-         'яна',
-         'слава'
-     ]
-    await bot. send_poll(
-        chat_id= message. chat. id,
-        question=question,
-        options=answer,
-        correct_option_id=1,
-        explanation ='мальчик',
-        type='quiz',
-        reply_markup=markup,
-    )
-
-@dp. callback_query_handler(lambda call: call.data == 'button_1')
-async def quiz_2(call: types.CallbackQuery):
-    question = "кто из них мульти миллиардер?"
-    answer = [
-        'Д.Вашингтон',
-        'У.Смит',
-        'С.Жапаров',
-        'P.Кийосаки',
-        'Джек Ма',
-
-
-    ]
-    await bot. send_poll(
-        chat_id=call.message.chat.id,
-        question=question,
-        options=answer,
-        correct_option_id=0,
-        type='quiz',
-
-
-
-    )
-
-
-@dp. message_handler(commands=['mem'])
-async def mem(message: types.Message):
-    photo = open("photo/2.jpg", "rb")
-
-
-    await bot.send_photo(message.from_user.id, photo=photo)
-
-@dp.message_handler(commands=['vetka'])
-async def vetka_quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-
-    button_call_5 = InlineKeyboardButton("Хорошо",
-
-                                         callback_data="button_call_5")
-
-    button_call_6 = InlineKeyboardButton("Плохо",
-
-                                         callback_data="button_call_6")
-
-    markup.add(button_call_5, button_call_6)
-
-    await bot.send_message(message.chat.id, 'Как настроение?',
-
-                           reply_markup=markup)
-@dp.callback_query_handler(lambda call:call.data=="button_call_5")
-
-async def vetka_quiz_good_1(call: types.CallbackQuery):
-
-
-    await bot.send_message(call.message.chat.id, 'Почему хорошо?')
-
-@dp.callback_query_handler(lambda call:call.data=="button_call_6")
-
-async def vetka_quiz_bad_1(call: types.CallbackQuery):
-
-
-
-
-    await bot.send_message(call.message.chat.id,",болит голова")
+admin.register_handlers_admin(dp)
+callback.register_handlers_callback(dp)
+client.register_handlers_client(dp)
+extra.register_handlers_extra(dp)
 
 
 
@@ -104,19 +15,10 @@ async def vetka_quiz_bad_1(call: types.CallbackQuery):
 
 
 
-@dp. message_handler()
-async def echo(message: types.Message):
-    if message.from_user.id == Admin:
-        games = ['🎲', '🏏', '⚽']
-        # value = random.choice(games)
-        if message.text.startswith('game'):
-            await bot.send_message(message.chat.id, random.choice(games))
-        if message.text.startswith('pin'):
-            await bot.pin_chat_message(message.chat.id, message.message_id)
-        await bot.send_message(message.chat.id, message.text)
-        a = int(message.text)
-        if a:
-            await bot.send_message(message.chat.id, a ** 2)
+
 
 if __name__ == "__main__":
     executor. start_polling(dp, skip_updates=True)
+
+
+
