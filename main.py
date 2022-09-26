1db950915a1a79f5aa34d2d402f03d3fd8c4b1c4
albello <kalbn@internet.ru>
2022-09-26 15:59:04 +0600

    Home Work 3

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import asyncio
+
 from aiogram import types
 from aiogram.utils import executor
 from config import bot, dp,Admin
-from handlers import admin,callback,client,extra,fsm
+from handlers import admin,callback,client,extra,fsm, notification
 from config import storage
 from database.bot_db import sql_create
 
 
 
 async def on_startup(_):
+    asyncio.create_task(notification.schedule())
     sql_create()
 
 
-
+notification.register_handlers_notification(dp)
 admin.register_handlers_admin(dp)
 callback.register_handlers_callback(dp)
 client.register_handlers_client(dp)
